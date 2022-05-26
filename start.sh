bc9943590991d6ffb42887dfa2009cfa28652cb0
Stillalive2 <88567635+Stillalive2@users.noreply.github.com>
2022-05-26 13:41:54 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo, Please Wait..."
-git clone -b master https://github.com/StillAlive2/MovieBot_OG.git /EvaMaria
-cd /EvaMaria
+git clone -b master https://github.com/StillAlive2/MovieBot_OG.git /MovieBot_OG
+cd /MovieBot_OG
 echo "Installing Requirements..."
 pip3 install -U -r requirements.txt
 echo "Starting Bot, Please Wait..."
